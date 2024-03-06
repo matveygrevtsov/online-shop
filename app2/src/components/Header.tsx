3d0faa0bd16278136f6a0a901d98b5e553eb5dec
Zar Zakharov <severenit@gmail.com>
2024-03-06 11:08:53 +0300

    Fix webpack config for app1 and app2, also fix lerna workspasec (#6)
    Co-authored-by: Zar Zakharov <sergey.zakharov@vk.team>

--- app2/src/components/Header.tsx
+++ app2/src/components/Header.tsx
@@ -1,62 +1,58 @@
-import React from "react";
-// import { Badge, Menu, MenuProps } from "antd";
-// import {
-//   UserAddOutlined,
-//   LoginOutlined,
-//   ShoppingOutlined,
-//   ShoppingCartOutlined,
-//   LogoutOutlined,
-// } from "@ant-design/icons";
-// import { useMemo } from "react";
+import React, { useMemo } from "react";
+import { Badge, Menu, MenuProps } from "antd";
+import {
+  UserAddOutlined,
+  LoginOutlined,
+  ShoppingOutlined,
+  ShoppingCartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
 
 const Header = () => {
-  // const items = useMemo(() => {
-  //   const items: MenuProps["items"] = [
-  //     {
-  //       label: "Зарегистрироваться",
-  //       key: "sign-up",
-  //       icon: <UserAddOutlined />,
-  //     },
-  //     {
-  //       label: "Войти",
-  //       key: "sign-in",
-  //       icon: <LoginOutlined />,
-  //     },
-  //     {
-  //       label: "Товары",
-  //       key: "main",
-  //       icon: <ShoppingOutlined />,
-  //     },
-  //     {
-  //       label: "Корзина",
-  //       key: "cart",
-  //       icon: (
-  //         <Badge count={5} size="small">
-  //           <ShoppingCartOutlined />
-  //         </Badge>
-  //       ),
-  //     },
-  //     {
-  //       label: "Выйти",
-  //       key: "logout",
-  //       icon: <LogoutOutlined />,
-  //     },
-  //   ];
+  const items = useMemo(() => {
+    const items: MenuProps["items"] = [
+      {
+        label: "Зарегистрироваться",
+        key: "sign-up",
+        icon: <UserAddOutlined />,
+      },
+      {
+        label: "Войти",
+        key: "sign-in",
+        icon: <LoginOutlined />,
+      },
+      {
+        label: "Товары",
+        key: "main",
+        icon: <ShoppingOutlined />,
+      },
+      {
+        label: "Корзина",
+        key: "cart",
+        icon: (
+          <Badge count={5} size="small">
+            <ShoppingCartOutlined />
+          </Badge>
+        ),
+      },
+      {
+        label: "Выйти",
+        key: "logout",
+        icon: <LogoutOutlined />,
+      },
+    ];
 
-  //   return items;
-  // }, []);
+    return items;
+  }, []);
 
-  // return (
-  //   <Menu
-  //     onClick={console.log}
-  //     selectedKeys={["main"]}
-  //     mode="horizontal"
-  //     items={items}
-  //   />
-  // );
-
-  // return <Badge>666</Badge>;
-  return <h1>Header</h1>;
+  return (
+    <Menu
+      onClick={console.log}
+      selectedKeys={["main"]}
+      mode="horizontal"
+      items={items}
+    />
+  );
 };
 
 export default Header;
